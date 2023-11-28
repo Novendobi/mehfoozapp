dd3f924f33b16f8434f97225813f801f5b8aa603
Novendobi <nchidinma4@gmail.com>
2023-11-28 14:53:27 +0100

    csrf

--- base/management/commands/start_app.py
+++ base/management/commands/start_app.py
@@ -2,6 +2,7 @@ import os
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from threading import Thread
+import time
 
 class Command(BaseCommand):
     help = 'Starts the Gunicorn server and adds the django-crontab jobs'
@@ -11,12 +12,11 @@ class Command(BaseCommand):
         # Add the django-crontab jobs
         #call_command('crontab', 'add')
         #call_command('crontab', 'show')
-        call_command('update_free_credits')
+        #call_command('update_free_credits')
 
         # Start the Gunicorn server in a new thread
         #Thread(target=os.system, args=('python manage.py runserver',)).start()
         Thread(target=os.system, args=('gunicorn smartlearning.wsgi',)).start()
-"""        while True:
+        while True:
             call_command('update_free_credits')
-            time.sleep(60)
-"""
+            time.sleep(120)
